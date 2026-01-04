e2a1ce46dba5b0714bf9df2a556cc134b7563bfc
amir <191767667+Amir123kaka@users.noreply.github.com>
2026-01-04 17:13:48 +0330

    create about home contact

--- home_app/views.py
+++ home_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+def home_view(request):
+    return render(request, "home.html")
+
+def about_view(request):
+    return render(request, "about.html")
+
+def contact_view(request):
+    return render(request, "contact.html")
